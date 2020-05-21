1bc14fc4253022c55b5cd0c15bcb078012a15b97
Danish Mujeeb <danish.mujeeb@gmail.com>
2020-05-21 17:49:38 -0400

    implementation of a full binary adder

---
+++ circuits/adder.go
@@ -0,0 +1,97 @@
+package circuits
+
+import (
+	"github.com/danishm/andornot/gates"
+)
+
+// FullAdder represents an interface exposed by a full binary adder
+type FullAdder interface {
+
+	// Inputs
+	A() chan int
+	B() chan int
+	CIn() chan int
+
+	// Outputs
+	S() chan int
+	COut() chan int
+
+	Stop()
+}
+
+type fullAdder struct {
+	a   chan int
+	b   chan int
+	cin chan int
+
+	xor1 gates.Gate
+	and1 gates.Gate
+	xor2 gates.Gate
+	and2 gates.Gate
+	or   gates.Gate
+
+	board gates.Board
+}
+
+// DefaultFullAdder creates and returns a full binary adder
+func DefaultFullAdder() FullAdder {
+
+	a := make(chan int)
+	b := make(chan int)
+	cin := make(chan int)
+
+	// Creating everything on an internal board
+	ib := gates.DefaultBoard()
+	xor1 := gates.XOR(ib)
+	xor2 := gates.XOR(ib)
+	and1 := gates.AND(ib)
+	and2 := gates.AND(ib)
+	or := gates.OR(ib)
+
+	// Making all the connections based on full adder implementation at
+	// https://www.electronics-tutorials.ws/combination/comb_7.html
+	ib.Connect(a, xor1.Pin1(), and1.Pin1())
+	ib.Connect(b, xor1.Pin2(), and1.Pin2())
+	ib.Connect(xor1.Out(), xor2.Pin1(), and2.Pin1())
+	ib.Connect(cin, xor2.Pin2(), and2.Pin2())
+	ib.Connect(and1.Out(), or.Pin1())
+	ib.Connect(and2.Out(), or.Pin2())
+
+	adder := fullAdder{
+		a:     a,
+		b:     b,
+		cin:   cin,
+		xor1:  xor1,
+		and1:  and1,
+		xor2:  xor2,
+		and2:  and2,
+		or:    or,
+		board: ib,
+	}
+
+	return &adder
+}
+
+func (fa *fullAdder) A() chan int {
+	return fa.a
+}
+
+func (fa *fullAdder) B() chan int {
+	return fa.b
+}
+
+func (fa *fullAdder) CIn() chan int {
+	return fa.cin
+}
+
+func (fa *fullAdder) S() chan int {
+	return fa.xor2.Out()
+}
+
+func (fa *fullAdder) COut() chan int {
+	return fa.or.Out()
+}
+
+func (fa *fullAdder) Stop() {
+	fa.board.Stop()
+}
---
+++ circuits/adder_test.go
@@ -0,0 +1,42 @@
+package circuits
+
+import (
+	"testing"
+)
+
+// TestXOR tests the XOR gate
+func TestXOR(t *testing.T) {
+	adder := DefaultFullAdder()
+
+	// This is basically the full truth table of the adder
+	cases := []struct {
+		a    int
+		b    int
+		cin  int
+		s    int
+		cout int
+	}{
+		{cin: 0, a: 0, b: 0, s: 0, cout: 0},
+		{cin: 0, a: 0, b: 1, s: 1, cout: 0},
+		{cin: 0, a: 1, b: 0, s: 1, cout: 0},
+		{cin: 0, a: 1, b: 1, s: 0, cout: 1},
+		{cin: 1, a: 0, b: 0, s: 1, cout: 0},
+		{cin: 1, a: 0, b: 1, s: 0, cout: 1},
+		{cin: 1, a: 1, b: 0, s: 0, cout: 1},
+		{cin: 1, a: 1, b: 1, s: 1, cout: 1},
+	}
+
+	for _, c := range cases {
+		adder.A() <- c.a
+		adder.B() <- c.b
+		adder.CIn() <- c.cin
+		actualS := <-adder.S()
+		actualCout := <-adder.COut()
+		if actualS != c.s || actualCout != c.cout {
+			t.Logf("CIn:%d A:%d B:%d -> Sum:%d Cout:%d  expected Sum:%d Cout:%d", c.cin, c.a, c.b, actualS, actualCout, c.s, c.cout)
+			t.Fail()
+		}
+	}
+
+	adder.Stop()
+}
